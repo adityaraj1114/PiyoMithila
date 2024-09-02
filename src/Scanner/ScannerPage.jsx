d43d43968f30cb47b7b759a025e9e49e746a93f2
[Aditya Muskan] <adityarajmdb1114@gmail.com>
2024-09-02 18:02:22 +0530

    ui changes

--- src/Scanner/ScannerPage.jsx
+++ src/Scanner/ScannerPage.jsx
@@ -19,9 +19,9 @@ export const ScannerPage = () => {
           </div>
 
           <div className="ownerDetail">
-          <h2>Shivam Kumar</h2>
+          <h2>Vijay Kumar</h2>
           <p>(Owner)</p>
-          <h3>Shivam Fancy Cracker</h3>
+          <h3>Piyo Mithila</h3>
           </div>
 
           <div className="scProfileImg">
